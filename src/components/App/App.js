19019795eccb5f676e17cd6263e6d8f5169ff750
nathanganesh <nathangemieee@gmail.com>
2020-07-18 10:53:20 +0200

    clickable items

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
+import Movie from '../Movie/Movie';
+import NotFound from '../notFound/NotFound';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<div>
-			<Header />
-			<Home />
-		</div>
+		<BrowserRouter>
+			<React.Fragment>
+				<Header />
+				<Switch>
+					<Route path="/" component={Home} exact />
+					<Route path="/:movieId" component={Movie} exact />
+					<Route component={NotFound} />
+				</Switch>
+			</React.Fragment>
+		</BrowserRouter>
 	);
 };
 
--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './MovieThumb.css';
+import { Link } from 'react-router-dom';
 
 const MovieThumb = (props) => {
 	return (
 		<div className="rmdb-moviethumb">
-			<img src={props.image} alt="movieThumb" />
+			{props.clickable ? (
+				<Link to={{ pathname: `/${props.movieId}`, movieName: `${props.movieName}` }}>
+					<img src={props.image} alt="moviethumb" />
+				</Link>
+			) : (
+				<img src={props.image} alt="moviethumb" />
+			)}
 		</div>
 	);
 };
